8ee5a294d3e9de2f01a4dfb43fa9df40cf53912e
yotvat <idooy11@gmail.com>
2024-02-13 17:23:45 +0200

    Implement onBall3Click and modify previews

--- js/main.js
+++ js/main.js
@@ -1,14 +1,46 @@
 'use strict'
 var ballSize = 100
-var maxDiameter = 200
-function onBallClick(elBall) {
+var maxDiameter = 400
+var elBall1 = document.querySelector('.ball')
+var elBall2 = document.querySelector('.second-ball')
+var elBall3 = document.querySelector('.third-ball')
+var ball1Size = 100
+var ball2Size = 100
+
+function onBall1Click() {
     var randColor = getRandomColor()
-    ballSize = getRandomInt(80,230)
-    if (ballSize > maxDiameter) {
-        ballSize = 100
+    ball1Size = getRandomInt(80,401)
+    if (ball1Size > maxDiameter) {
+        ball1Size = 100
     }
-    elBall.style.width = ballSize + 'px'
-    elBall.style.height = ballSize + 'px'
-    elBall.innerHTML = ballSize
-    elBall.style.backgroundColor = randColor
+    elBall1.style.width = ball1Size + 'px'
+    elBall1.style.height = ball1Size + 'px'
+    elBall1.innerHTML = ball1Size
+    elBall1.style.backgroundColor = randColor
+}
+
+function onBall2Click() {
+    var randColor = getRandomColor()
+    ball2Size = getRandomInt(80,401)
+    if (ball2Size > maxDiameter) {
+        ball2Size = 100
+    }
+    elBall2.style.width = ball2Size + 'px'
+    elBall2.style.height = ball2Size + 'px'
+    elBall2.innerHTML = ball2Size
+    elBall2.style.backgroundColor = randColor
+}
+
+function onBall3Click(elball){
+    var ball1Color = elBall2.style.backgroundColor
+    var ball2Color = elBall1.style.backgroundColor
+    elBall1.style.width = ball2Size + 'px'
+    elBall1.style.height = ball2Size + 'px'
+    elBall1.innerHTML = ball2Size
+    elBall1.style.backgroundColor = ball1Color
+
+    elBall2.style.width = ball1Size + 'px'
+    elBall2.style.height = ball1Size + 'px'
+    elBall2.innerHTML = ball1Size
+    elBall2.style.backgroundColor = ball2Color
 }
