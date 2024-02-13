210689385e70ef98b19f14bae53d3d66efa6650f
yotvat <idooy11@gmail.com>
2024-02-13 15:55:49 +0200

    Modify onBallClick for max ball size

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 var ballSize = 100
 function onBallClick(elBall) {
     ballSize = ballSize + 50
+    if (ballSize > 400) {
+        ballSize = 100
+    }
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.innerHTML = ballSize
