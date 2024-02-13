f857f099073dec58b8a064a30a33a7cb8418175f
yotvat <idooy11@gmail.com>
2024-02-13 16:21:27 +0200

    Modify onBallClick

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 'use strict'
 var ballSize = 100
 function onBallClick(elBall) {
-    ballSize = ballSize + 50
-    if (ballSize > 400) {
-        ballSize = 100
-    }
+    ballSize = getRandomInt(20,61)
+    // if (ballSize > 400) {
+    //     ballSize = 100
+    // }
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.innerHTML = ballSize
