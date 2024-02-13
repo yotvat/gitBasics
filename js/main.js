49de2b3c663d9c4a26b9f9073302ab7bffc7fc78
yotvat <idooy11@gmail.com>
2024-02-13 16:35:02 +0200

    Add maxDiameter var and implement in the function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 'use strict'
 var ballSize = 100
+var maxDiameter = 200
 function onBallClick(elBall) {
     var randColor = getRandomColor()
-    ballSize = getRandomInt(20,61)
-    // if (ballSize > 400) {
-    //     ballSize = 100
-    // }
+    ballSize = getRandomInt(80,230)
+    if (ballSize > maxDiameter) {
+        ballSize = 100
+    }
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.innerHTML = ballSize
