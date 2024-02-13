771538bdd4ac36625d3220e733a70c1e21f09322
yotvat <idooy11@gmail.com>
2024-02-13 18:51:57 +0200

    Bug fixes-function ball3

--- js/main.js
+++ js/main.js
@@ -39,14 +39,18 @@ function onBall2Click() {
 function onBall3Click(){
     var ball1Color = elBall2.style.backgroundColor
     var ball2Color = elBall1.style.backgroundColor
-    elBall1.style.width = ball2Size + 'px'
-    elBall1.style.height = ball2Size + 'px'
-    elBall1.innerHTML = ball2Size
+    var prevBall1Size = ball1Size
+    ball1Size = ball2Size
+    ball2Size = prevBall1Size
+
+    elBall1.style.width = ball1Size + 'px'
+    elBall1.style.height = ball1Size + 'px'
+    elBall1.innerHTML = ball1Size
     elBall1.style.backgroundColor = ball1Color
 
-    elBall2.style.width = ball1Size + 'px'
-    elBall2.style.height = ball1Size + 'px'
-    elBall2.innerHTML = ball1Size
+    elBall2.style.width = ball2Size + 'px'
+    elBall2.style.height = ball2Size + 'px'
+    elBall2.innerHTML = ball2Size
     elBall2.style.backgroundColor = ball2Color
 }
 
@@ -86,9 +90,4 @@ function onBall6Click(){
     elBall2.style.height = 100 + 'px'
     elBall2.innerHTML = 100
     elBall2.style.backgroundColor = 'green'
-
-    
-
-
-    
 }
